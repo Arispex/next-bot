b394ab10e5e9fece069328ffb21391150aa53f5a
arispex <arispex@outlook.com>
2026-02-07 14:24:42 +0800

    Add menu plugin with category commands and permissions

---
+++ next_bot/plugins/menu.py
@@ -0,0 +1,143 @@
+from nonebot import on_command
+from nonebot.adapters import Bot, Event, Message
+from nonebot.params import CommandArg
+from next_bot.permissions import require_permission
+
+menu_matcher = on_command("菜单")
+basic_matcher = on_command("基础功能")
+group_matcher = on_command("身份组管理")
+permission_matcher = on_command("权限管理")
+server_matcher = on_command("服务器管理")
+user_matcher = on_command("用户管理")
+agent_matcher = on_command("代理功能")
+
+MENU_USAGE = "格式错误，正确格式：菜单"
+BASIC_USAGE = "格式错误，正确格式：基础功能"
+GROUP_USAGE = "格式错误，正确格式：身份组管理"
+PERMISSION_USAGE = "格式错误，正确格式：权限管理"
+SERVER_USAGE = "格式错误，正确格式：服务器管理"
+USER_USAGE = "格式错误，正确格式：用户管理"
+AGENT_USAGE = "格式错误，正确格式：代理功能"
+
+MENU_TEXT = "\n".join(
+    [
+        "基础功能",
+        "身份组管理",
+        "权限管理",
+        "服务器管理",
+        "用户管理",
+        "代理功能",
+    ]
+)
+
+BASIC_TEXT = "\n".join(["在线", "执行 <服务器 ID> <命令>"])
+GROUP_TEXT = "\n".join(
+    [
+        "身份组列表",
+        "添加身份组 <新身份组名称>",
+        "删除身份组 <要删除的身份组名称>",
+        "继承身份组 <身份组名称> <要继承的身份组名称>",
+        "取消继承身份组 <身份组名称>",
+        "添加身份组权限 <身份组名称> <权限名称>",
+        "删除身份组权限 <身份组名称> <权限名称>",
+    ]
+)
+PERMISSION_TEXT = "\n".join(
+    [
+        "添加用户权限 <用户 ID> <权限名称>",
+        "删除用户权限 <用户 ID> <权限名称>",
+        "修改用户身份组 <用户 ID> <身份组>",
+    ]
+)
+SERVER_TEXT = "\n".join(
+    [
+        "添加服务器 <显示的服务器名称> <IP> <游戏端口> <RestAPI 端口> <RestAPI Token>",
+        "删除服务器 <服务器 ID>",
+        "服务器列表",
+        "测试连通性 <服务器 ID>",
+    ]
+)
+USER_TEXT = "\n".join(["注册账号 <游戏名称>", "同步白名单"])
+AGENT_TEXT = "\n".join(["代理 <内容>", "允许执行命令", "拒绝执行命令"])
+
+
+def _parse_args(arg: Message) -> list[str]:
+    return [item for item in arg.extract_plain_text().strip().split() if item]
+
+
+@menu_matcher.handle()
+@require_permission("mn.menu")
+async def handle_menu(bot: Bot, event: Event, arg: Message = CommandArg()):
+    args = _parse_args(arg)
+    if args:
+        await bot.send(event, MENU_USAGE)
+        return
+    await bot.send(event, MENU_TEXT)
+
+
+@basic_matcher.handle()
+@require_permission("mn.basic")
+async def handle_basic_menu(
+    bot: Bot, event: Event, arg: Message = CommandArg()
+):
+    args = _parse_args(arg)
+    if args:
+        await bot.send(event, BASIC_USAGE)
+        return
+    await bot.send(event, BASIC_TEXT)
+
+
+@group_matcher.handle()
+@require_permission("mn.group")
+async def handle_group_menu(
+    bot: Bot, event: Event, arg: Message = CommandArg()
+):
+    args = _parse_args(arg)
+    if args:
+        await bot.send(event, GROUP_USAGE)
+        return
+    await bot.send(event, GROUP_TEXT)
+
+
+@permission_matcher.handle()
+@require_permission("mn.permission")
+async def handle_permission_menu(
+    bot: Bot, event: Event, arg: Message = CommandArg()
+):
+    args = _parse_args(arg)
+    if args:
+        await bot.send(event, PERMISSION_USAGE)
+        return
+    await bot.send(event, PERMISSION_TEXT)
+
+
+@server_matcher.handle()
+@require_permission("mn.server")
+async def handle_server_menu(
+    bot: Bot, event: Event, arg: Message = CommandArg()
+):
+    args = _parse_args(arg)
+    if args:
+        await bot.send(event, SERVER_USAGE)
+        return
+    await bot.send(event, SERVER_TEXT)
+
+
+@user_matcher.handle()
+@require_permission("mn.user")
+async def handle_user_menu(bot: Bot, event: Event, arg: Message = CommandArg()):
+    args = _parse_args(arg)
+    if args:
+        await bot.send(event, USER_USAGE)
+        return
+    await bot.send(event, USER_TEXT)
+
+
+@agent_matcher.handle()
+@require_permission("mn.agent")
+async def handle_agent_menu(bot: Bot, event: Event, arg: Message = CommandArg()):
+    args = _parse_args(arg)
+    if args:
+        await bot.send(event, AGENT_USAGE)
+        return
+    await bot.send(event, AGENT_TEXT)
